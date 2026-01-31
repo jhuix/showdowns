11e1e7be093b3d1b2afa20dda68c7313840a2e4d
jhuix <jhuix@sohu.com>
2026-01-31 17:37:48 +0800

    Update tester of toc and directive

--- src/extensions/showdown-directive.js
+++ src/extensions/showdown-directive.js
@@ -18,18 +18,16 @@ const sgvSymbols = [
   '<symbol id="icon-pro-tip" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M6.5 0C3.48 0 1 2.19 1 5c0 .92.55 2.25 1 3 1.34 2.25 1.78 2.78 2 4v1h5v-1c.22-1.22.66-1.75 2-4 .45-.75 1-2.08 1-3 0-2.81-2.48-5-5.5-5zm3.64 7.48c-.25.44-.47.8-.67 1.11-.86 1.41-1.25 2.06-1.45 3.23-.02.05-.02.11-.02.17H5c0-.06 0-.13-.02-.17-.2-1.17-.59-1.83-1.45-3.23-.2-.31-.42-.67-.67-1.11C2.44 6.78 2 5.65 2 5c0-2.2 2.02-4 4.5-4 1.22 0 2.36.42 3.22 1.19C10.55 2.94 11 3.94 11 5c0 .66-.44 1.78-.86 2.48zM4 14h5c-.23 1.14-1.3 2-2.5 2s-2.27-.86-2.5-2z"></path></symbol>',
   '<symbol id="icon-pro-info" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></symbol>',
   '<symbol id="icon-pro-warning" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"></path></symbol>',
-  '<symbol id="icon-pro-danger" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M5.05.31c.81 2.17.41 3.38-.52 4.31C3.55 5.67 1.98 6.45.9 7.98c-1.45 2.05-1.7 6.53 3.53 7.7-2.2-1.16-2.67-4.52-.3-6.61-.61 2.03.53 3.33 1.94 2.86 1.39-.47 2.3.53 2.27 1.67-.02.78-.31 1.44-1.13 1.81 3.42-.59 4.78-3.42 4.78-5.56 0-2.84-2.53-3.22-1.25-5.61-1.52.13-2.03 1.13-1.89 2.75.09 1.08-1.02 1.8-1.86 1.33-.67-.41-.66-1.19-.06-1.78C8.18 5.31 8.68 2.45 5.05.32L5.03.3l.02.01z"></path></symbol>'
+  '<symbol id="icon-pro-danger" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M5.05.31c.81 2.17.41 3.38-.52 4.31C3.55 5.67 1.98 6.45.9 7.98c-1.45 2.05-1.7 6.53 3.53 7.7-2.2-1.16-2.67-4.52-.3-6.61-.61 2.03.53 3.33 1.94 2.86 1.39-.47 2.3.53 2.27 1.67-.02.78-.31 1.44-1.13 1.81 3.42-.59 4.78-3.42 4.78-5.56 0-2.84-2.53-3.22-1.25-5.61-1.52.13-2.03 1.13-1.89 2.75.09 1.08-1.02 1.8-1.86 1.33-.67-.41-.66-1.19-.06-1.78C8.18 5.31 8.68 2.45 5.05.32L5.03.3l.02.01z"></path></symbol>',
 ];
 
 function splitAndStripQuotes(str) {
-  return str.match(/(?:[^\s"']+|"[^"]*"|'[^']*')+/g)
-    .map(token => {
-      if ((token.startsWith('"') && token.endsWith('"')) ||
-        (token.startsWith("'") && token.endsWith("'"))) {
-        return token.slice(1, -1);
-      }
-      return token;
-    });
+  return str.match(/(?:[^\s"']+|"[^"]*"|'[^']*')+/g).map((token) => {
+    if ((token.startsWith('"') && token.endsWith('"')) || (token.startsWith("'") && token.endsWith("'"))) {
+      return token.slice(1, -1);
+    }
+    return token;
+  });
 }
 
 function parseSvgType(name) {
@@ -79,7 +77,7 @@ function parseAttribute(attribute, container, exclude) {
               if (classname && !obj.classList.includes(classname)) {
                 obj.classList.push(classname);
               }
-            })
+            });
           } else {
             classes.forEach((classname) => {
               if (classname) {
@@ -91,7 +89,7 @@ function parseAttribute(attribute, container, exclude) {
                   obj.classList.push(classname);
                 }
               }
-            })
+            });
           }
         }
       } else {
@@ -119,7 +117,7 @@ function parseAttribute(attribute, container, exclude) {
         }
       }
     }
-  })
+  });
   return obj;
 }
 
@@ -158,7 +156,7 @@ function showdownDirective() {
             /^ {0,3}(:::+)[ \t]*([-\w]+)[ \t]*(?:([^:\f\v\r\n\[\]\{\}]*)|(?:\[([^\[\]]*)\])?[ \t]*(?:\{([^\{\}]*)\})?)[: \t]*\n([\s\S]*?)\n {0,3}\1[: \t]*/gm,
             function (wholeMatch, delim, name, title0, title, attribute, content) {
               const container = {
-                classList: ['showdown-container']
+                classList: ['showdown-container'],
               };
               let id = '';
               let attrs = '';
@@ -224,11 +222,12 @@ function showdownDirective() {
               }
               const code = `<div ${id}class="${container.classList.join(' ')}"${attrs}>${title}${content}</div>`;
               return showdown.subParser('hashBlock')(code, options, globals);
-            }
-          )
+            },
+          );
 
           // leaf directive
-          text = text.replace(/^ {0,3}(?<!:)::[ \t]*([-\w]+)[ \t]*(?:\[([^\[\]]*)\])?[ \t]*\{([^\{\}]*)\}[ \t]*$/gm,
+          text = text.replace(
+            /^ {0,3}(?<!:)::[ \t]*([-\w]+)[ \t]*(?:\[([^\[\]]*)\])?[ \t]*\{([^\{\}]*)\}[ \t]*$/gm,
             function (wholeMatch, name, title, attribute) {
               const leaf = {};
               parseAttribute(attribute, leaf);
@@ -249,9 +248,9 @@ function showdownDirective() {
                   attrs = attributeToString(leaf.attribute);
                 }
                 if (leaf.classList) {
-                  className = ` class="${leaf.classList.join(' ')}"`
+                  className = ` class="${leaf.classList.join(' ')}"`;
                 }
-                const code = `<iframe ${id}${className}${attrs}>${title}}</iframe>`
+                const code = `<iframe ${id}${className}${attrs}>${title}}</iframe>`;
                 return showdown.subParser('hashBlock')(code, options, globals);
               }
 
@@ -264,7 +263,7 @@ function showdownDirective() {
                 if (leaf.attribute) {
                   attrs = attributeToString(leaf.attribute);
                 }
-                const code = `<link ${id}${attrs}>`
+                const code = `<link ${id}${attrs}>`;
                 return showdown.subParser('hashBlock')(code, options, globals);
               }
 
@@ -280,7 +279,7 @@ function showdownDirective() {
                 if (leaf.attribute) {
                   attrs = attributeToString(leaf.attribute);
                 }
-                const code = `<style ${id}${attrs}>${title}</style>`
+                const code = `<style ${id}${attrs}>${title}</style>`;
                 return showdown.subParser('hashBlock')(code, options, globals);
               }
 
@@ -290,11 +289,15 @@ function showdownDirective() {
                 }
               };
               EventBus.emit(leafDirectiveEventName, name, title, leaf, callback);
-            });
+            },
+          );
 
           // text directive
-          text = text.replace(/(?<!:):([-\w]+)\[([^\[\]]*)\](?:\{([^\{\}]*)\})?/g,
+          text = text.replace(
+            /(?<!:):([-\w]+)\[([^\[\]]*)\]\{([^\{\}]*)\}/g,
             function (wholeMatch, name, title, attribute) {
+              const leaf = {};
+              parseAttribute(attribute, leaf);
               let id = '';
               let attrs = '';
               if (leaf.id) {
@@ -303,118 +306,30 @@ function showdownDirective() {
               if (leaf.attribute) {
                 attrs = attributeToString(leaf.attribute);
               }
-              if (!title) {
-                title = '';
-              }
-              if (name === 'css-link') {
-                const code = `<link ${id}${attrs}>`
-                return showdown.subParser('hashBlock')(code, options, globals);
-              }
-
-              if (name === 'css' && title) {
-                const code = `<style ${id}${attrs}>${title}</style>`
-                return showdown.subParser('hashBlock')(code, options, globals);
-              }
-
               let className = '';
               if (leaf.classList) {
-                className = ` class="${leaf.classList.join(' ')}"`
+                className = ` class="${leaf.classList.join(' ')}"`;
               }
               if (title) {
                 title = showdown.subParser('githubCodeBlocks')(title, options, globals);
                 title = showdown.subParser('blockGamut')(title, options, globals);
+              } else {
+                title = '';
               }
               const code = `<${name} ${id}${className}${attrs}>${title}</${name}>`;
               return showdown.subParser('hashBlock')(code, options, globals);
-            });
+            },
+          );
 
           // attacklab: strip sentinel
           text = text.replace(/¨0/, '');
 
           return converter._dispatch('directive.after', text, options, globals);
-        }
-      }
-    }
+        },
+      },
+    },
   ];
 }
-
-
-// function showdownContainer() {
-//   return [
-//     {
-//       type: 'lang',
-//       filter: (text, converter) =>
-//         text.replace(
-//           /^ {0,3}(:::+)(?: +([\S]+)[ \t]*([^\s][\S\t ]*)|([^\[\]\{\}\s]+)(?:\[([ \t\S]*)\]){0,1}(?:\{([ \t\S]*)\}){0,1})\n([\s\S]*?)\n {0,3}\1[ \t]*$/gm,
-//           function (wholeMatch, delim, classname0, title0, name, title, attribute, content) {
-//             const classnames = ['showdown-container'];
-//             let id = '';
-//             let style = '';
-//             let svgContent = '';
-//             if (classname0) {
-//               const classes = classname0.split('-');
-//               if (!classes.includes('alert')) {
-//                 classnames.push('note');
-//               }
-//               classnames.push(...classes);
-//               title = title0 ?? '';
-//             } else if (name) {
-//               let obj;
-//               switch (name) {
-//                 case 'note':
-//                 case '备注':
-//                   obj = parseAttribute(attribute, '.alert');
-//                   classnames.push('note');
-//                   break;
-//                 case 'alert':
-//                 case '注意':
-//                   classnames.push('alert');
-//                   obj = parseAttribute(attribute, '.note');
-//                   break;
-//                 default:
-//                   obj = parseAttribute(attribute);
-//                   obj.svgType = parseSvgType(name);
-//               }
-//               if (!obj.svgType) {
-//                 obj.svgType = { id: 'icon-pro-note', type: 'success' };
-//               }
-
-//               if (!obj.classes.includes('alert') && !classnames.includes('alert')) {
-//                 if (!obj.classes.includes('note') && !classnames.includes('note')) {
-//                   classnames.push('note');
-//                 }
-//               }
-//               if (obj.classes.length > 0) {
-//                 classnames.push(...obj.classes);
-//               }
-//               if (!obj.classes.includes(obj.svgType.type)) {
-//                 classnames.push(obj.svgType.type);
-//               }
-//               if (obj.ids.length > 0) {
-//                 id = `id="${obj.ids.join(' ')}" `;
-//               }
-//               if (obj.style) {
-//                 style = ` style="${obj.style}"`;
-//               }
-//               svgContent = `<span class="container-icon"><svg xmlns="http://www.w3.org/2000/svg"><use xlink:href="#${obj.svgType.id}"/></svg></span>`;
-//             }
-//             if (title) {
-//               title = converter.makeHtml(title).replace(/^<p>(.*)<\/p>$/, '$1');
-//               title = `<div class="container-title">${svgContent}${title}</div>`;
-//             } else {
-//               title = ''
-//             }
-//             if (content) {
-//               content = `<div class="container-content">${converter.makeHtml(content)}</div>`;
-//             } else {
-//               content = '';
-//             }
-//             return `<div ${id}class="${classnames.join(' ')}"${style}>${title}${content}</div>`;
-//           }
-//         ),
-//     },
-//   ];
-// }
 
 function showdownAsyncDirective() {
   return [
@@ -425,8 +340,8 @@ function showdownAsyncDirective() {
           obj.symbols.push(...sgvSymbols);
         }
         return obj;
-      }
-    }
+      },
+    },
   ];
 }
 
